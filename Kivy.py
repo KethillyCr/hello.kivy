05abeebf98c9f22fcfd972785e0856e15c021b8a
kethilly <kethillycrystiny07@gmail.com>
2024-04-17 16:40:17 -0300

    criando kivy

---
+++ Kivy.py
@@ -0,0 +1,7 @@
+from kivy.app import App 
+from kivy.uix.button import Button
+class myApp(App):
+    def build(self):
+        return Button(text="Hello World! This is my first progam in kivy \n I'm a SESIANO student, and i love my Teacher")
+if __name__ == '__main__':
+    myApp().run()
